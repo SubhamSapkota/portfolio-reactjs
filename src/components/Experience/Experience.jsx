fa4564d86f01495d61a4d17327386d321c012497
Subham Sapkota <shuvam760@gmail.com>
2026-01-20 12:37:14 +0545

    Update experience counts in Experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
     <div className="experience" id='Experience'>
     <div className="achievement">
       {/* darkMode */}
-      <div className="circle" style={{color: darkMode?'var(--orange)':''}}>0+</div>
+      <div className="circle" style={{color: darkMode?'var(--orange)':''}}>1+</div>
       <span  style={{color: darkMode?'white':''}}>years </span>
       <span>Experience</span>
     </div>
@@ -18,7 +18,7 @@ const Experience = () => {
       <span>Projects</span>
     </div>
     <div className="achievement">
-      <div className="circle" style={{color: darkMode?'var(--orange)':''}}>0</div>
+      <div className="circle" style={{color: darkMode?'var(--orange)':''}}>3</div>
       <span  style={{color: darkMode?'white':''}}>company </span>
       <span>worked</span>
     </div>
